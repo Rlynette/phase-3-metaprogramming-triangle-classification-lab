dcc7a0bdd0ec5429b75030d9fc02efa17b7300e5
Rlynette <lynette.ronnoh@student.moringaschool.com>
2022-10-25 11:51:03 +0300

    Completed Assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
   # write code here
+  def initialize(a, b, c)
+    @a = a
+    @b =b
+    @c = c
+  end
+
+  def kind
+    if (@a <= 0 || @b <= 0 || @c <=0)
+      raise TriangleError
+    elsif (@a + @b <= @c || @b + @c <= @a || @a + @c <= @b)
+      raise TriangleError
+      elsif (@a == @b && @b == @c && @a == @c)
+        :equilateral
+        elsif ( @a == @b || @a == @c || @b == @c)
+          :isosceles
+        else
+          :scalene
+        end
+      end
+
+  class TriangleError < StandardError
+    def message
+      "triangle error code"
+    end
+end
 end
